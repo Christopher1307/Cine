21901bd84614dff8f638d2c10cc517f115ea5895
Christopher <araujochristopher715@gmail.com>
2024-12-08 19:39:49 +0100

    aaaaaaaaaaaaaaa

--- app/src/main/java/com/example/cine/Activitys/SettingsActivity.kt
+++ app/src/main/java/com/example/cine/Activitys/SettingsActivity.kt
@@ -10,7 +10,7 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        
+
         // Configurar la barra de herramientas
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
